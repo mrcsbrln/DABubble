43877125f7e70c6bfd762d57174bd6d3b0bbcdd6
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-04-04 23:36:46 +0200

    progress

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,5 +7,20 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "dabubble403", appId: "1:737440413813:web:5f70fada3aa89cee1c203c", storageBucket: "dabubble403.firebasestorage.app", apiKey: "AIzaSyBAHM6tfKP6DH0U5UvZu-zwJcrRGiHW7HE", authDomain: "dabubble403.firebaseapp.com", messagingSenderId: "737440413813" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'dabubble403',
+        appId: '1:737440413813:web:5f70fada3aa89cee1c203c',
+        storageBucket: 'dabubble403.firebasestorage.app',
+        apiKey: '',
+        authDomain: 'dabubble403.firebaseapp.com',
+        messagingSenderId: '737440413813',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
