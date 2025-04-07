2ffc3eaedf7c7604231beb4c5017f6f2cca22c16
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-04-07 17:11:37 +0200

    set up firebase auth register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+];
--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../services/auth/auth.service';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  authService = inject(AuthService);
+  formBuilder = inject(FormBuilder);
+  router = inject(Router);
 
+  errorMessage: string | null = null;
+
+  form = this.formBuilder.nonNullable.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    passwort: ['', Validators.required],
+  });
+
+  onSubmit(): void {
+    const rawForm = this.form.getRawValue();
+    this.authService
+      .register(rawForm.email, rawForm.passwort, rawForm.username)
+      .subscribe({
+        next: () => {
+          this.router.navigateByUrl('/');
+        },
+        error: (err) => {
+          this.errorMessage = err.code;
+        },
+      });
+  }
 }
