ef4c6ebf17cd86475db96f06d876eb10d99ba1a7
Daniele Dona <d.dona01@gmail.com>
2025-04-09 22:13:23 +0200

    Added components header,chat and thread

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,9 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
+import { HeaderComponent } from './header/header.component';
+import { ChatComponent } from './chat/chat.component';
+import { ThreadComponent } from './thread/thread.component';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [HeaderComponent, ChatComponent, ThreadComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
