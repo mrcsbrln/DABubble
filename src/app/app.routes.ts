d92f0efce41dbdb4232b188657c0b6e3b0732034
Daniele Dona <d.dona01@gmail.com>
2025-04-09 22:16:45 +0200

    Added routes MainComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
 
 export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  { path: '', component: MainComponent },
 ];
