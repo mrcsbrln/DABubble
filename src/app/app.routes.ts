328924d5113ae71ac826901f91eba8da6b2dda55
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-04-09 23:29:03 +0200

    progress

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,8 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { LoginComponent } from './components/auth/login/login.component';
-import { AppComponent } from './app.component';
-import { MainComponent } from './components/main/main.component';
 
 export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: '', component: MainComponent },
 ];
