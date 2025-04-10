4b5ad46ca54763d0f5ac4e0c4b63df7e8e82e44f
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-04-10 21:27:13 +0200

    configure routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,10 @@ import { Routes } from '@angular/router';
 import { authGuard } from './guards/auth.guard';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { LoginComponent } from './components/auth/login/login.component';
-import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
 
 export const routes: Routes = [
-  { path: '', component: AppComponent, canActivate: [authGuard] },
+  { path: '', component: MainComponent, canActivate: [authGuard] },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
 ];
--- src/app/components/main/header/header.component.ts
+++ src/app/components/main/header/header.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../services/auth/auth.service';
 
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  authService = inject(AuthService);
 }
