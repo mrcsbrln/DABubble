ed3011ac15228ce620d371cb6879fc48c95e1779
Daniele Dona <d.dona01@gmail.com>
2025-04-14 13:50:29 +0200

    Responsiv display grid layout

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -4,13 +4,32 @@ import { HeaderComponent } from './header/header.component';
 import { ChatComponent } from './chat/chat.component';
 import { ThreadComponent } from './thread/thread.component';
 import { WorkspaceComponent } from './workspace/workspace.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main',
-  imports: [HeaderComponent, WorkspaceComponent, ChatComponent, ThreadComponent],
+  imports: [
+    HeaderComponent,
+    WorkspaceComponent,
+    ChatComponent,
+    ThreadComponent,
+    CommonModule,
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
+
 export class MainComponent {
   authService = inject(AuthService);
+  isWorkspaceHidden: boolean = false;
+  isThreadHidden: boolean = false;
+
+  toggleWorkspaceVisibility(): void {
+    this.isWorkspaceHidden = !this.isWorkspaceHidden;
+  }
+
+  toggleThreadVisibility(): void {
+    this.isThreadHidden = !this.isThreadHidden;
+  }
+
 }
