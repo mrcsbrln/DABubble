e2dfb6280001f2c84c4a13aa1f5995a18d5115a2
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-04-14 17:08:25 +0200

    progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
-import { AuthService } from './services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +9,4 @@ import { AuthService } from './services/auth/auth.service';
 })
 export class AppComponent {
   title = 'DABubble';
-  authService = inject(AuthService);
-
-  ngOnInit(): void {
-    // this.authService.user$.subscribe((user) => {
-    //   if (user) {
-    //     this.authService.currentUser.set({
-    //       email: user.email!,
-    //       username: user.displayName!,
-    //     });
-    //   } else {
-    //     this.authService.currentUser.set(null);
-    //   }
-    // });
-  }
 }
