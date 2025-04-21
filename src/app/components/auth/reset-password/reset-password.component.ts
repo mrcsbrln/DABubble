614d263e7c4b29e7c7ba57ad2babb70a75b0cf0c
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-04-21 16:26:16 +0200

    progress reset password

--- src/app/components/auth/reset-password/reset-password.component.ts
+++ src/app/components/auth/reset-password/reset-password.component.ts
@@ -6,6 +6,7 @@ import {
   AbstractControl,
 } from '@angular/forms';
 import { AuthService } from '../../../services/auth/auth.service';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-reset-password',
@@ -16,11 +17,14 @@ import { AuthService } from '../../../services/auth/auth.service';
 export class ResetPasswordComponent {
   fb = inject(FormBuilder);
   authService = inject(AuthService);
+  router = inject(Router);
 
   form = this.fb.nonNullable.group({
     password1: ['', Validators.required],
     password2: ['', Validators.required],
   });
+
+  showConfirmation = signal(false);
 
   errorMessage: string | null = null;
 
@@ -42,6 +46,11 @@ export class ResetPasswordComponent {
     const newPassword = this.form.controls.password1.value;
     if (currentUser && this.comparePasswords()) {
       this.authService.updateUserPassword(currentUser, newPassword);
+      this.showConfirmation.set(true);
+      setTimeout(() => {
+        this.showConfirmation.set(false);
+        this.router.navigateByUrl('/login');
+      }, 1000);
     }
   }
 }
