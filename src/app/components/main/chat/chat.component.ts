0249fe9b88b1f4d50020b80abea7f8b444b9ed58
Daniele Dona <d.dona01@gmail.com>
2025-04-23 22:53:48 +0200

    Added send outgoing message

--- src/app/components/main/chat/chat.component.ts
+++ src/app/components/main/chat/chat.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 import { UserProfileComponent } from '../user-profile/user-profile.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
-  imports: [UserProfileComponent],
+  imports: [UserProfileComponent, FormsModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss',
 })
-export class ChatComponent {}
+export class ChatComponent {
+  newMessage: string = '';
+  messages: { text: string; outgoing: boolean }[] = [];
+
+
+
+  sendMessage(): void {
+    if (this.newMessage.trim() !== '') {
+      this.messages.push({ text: this.newMessage, outgoing: true });
+      this.newMessage = '';
+    }
+    console.log(this.messages);
+  }
+}
