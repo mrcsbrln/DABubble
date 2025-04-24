60269fe9d0265fa27e71ef119a98f9d5499968cf
Daniele Dona <d.dona01@gmail.com>
2025-04-24 20:53:56 +0200

    Added timestamp

--- src/app/components/main/chat/chat.component.ts
+++ src/app/components/main/chat/chat.component.ts
@@ -3,11 +3,12 @@ import { UserProfileComponent } from '../user-profile/user-profile.component';
 import { FormsModule } from '@angular/forms';
 import { Message } from '../../../interfaces/message.interface';
 import { UserService } from '../../../services/user.service';
-
+import { Timestamp } from '@angular/fire/firestore'; // Importiere Timestamp
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-chat',
-  imports: [UserProfileComponent, FormsModule],
+  imports: [UserProfileComponent, FormsModule, DatePipe],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss',
 })
@@ -16,16 +17,22 @@ export class ChatComponent {
   currentUser = this.userService.currentUserData;
 
   displayName = computed(() => this.currentUser()?.displayName);
-  // userUid = computed(() => this.currentUser()?.uid);
+  userUid = computed(() => this.currentUser()?.uid);
 
   newMessage: string = '';
   messages: Message[] = [];
 
   sendMessage(): void {
     if (this.newMessage.trim() !== '') {
-      this.messages.push({ username: this.displayName() });
+      const timestamp = Timestamp.now(); // Erzeuge einen Zeitstempel
+      this.messages.push({
+        username: this.displayName(),
+        content: this.newMessage,
+        timestamp: timestamp, // Füge den Zeitstempel hinzu
+      });
       this.newMessage = '';
     }
     console.log(this.messages);
+    console.log(this.userUid());
   }
 }
