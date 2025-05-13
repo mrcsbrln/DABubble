0226af7ec6ed26e87c01fbf7f4197764cdd53118
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-05-13 17:07:41 +0200

    set up user-profile overlay

--- src/app/components/main/user-profile/user-profile.component.ts
+++ src/app/components/main/user-profile/user-profile.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Output, signal } from '@angular/core';
 import { UserService } from '../../../services/user.service';
+import { AuthService } from '../../../services/auth/auth.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -10,6 +11,19 @@ import { UserService } from '../../../services/user.service';
 export class UserProfileComponent {
   @Output() closeDialog = new EventEmitter<void>();
   private userService = inject(UserService);
+  private authService = inject(AuthService);
+
+  isEditMode = signal(false);
+
+  getCurrentUserInUserCollection() {
+    return this.userService.users.find(
+      (user) => user.uid === this.authService.currentUser()?.uid
+    );
+  }
+
+  toggleEditMode() {
+    this.isEditMode.update((value) => !value);
+  }
 
   onCloseDialog() {
     this.closeDialog.emit();
