083dff5201e3ca2a5ef815b2fa84df55186c6bb6
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-05-18 19:15:18 +0200

    implement visibilty function workspace and thread

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth/auth.service';
 import { HeaderComponent } from './header/header.component';
 import { ThreadComponent } from './thread/thread.component';
@@ -7,7 +8,13 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main',
-  imports: [HeaderComponent, WorkspaceComponent, ThreadComponent, RouterOutlet],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    WorkspaceComponent,
+    ThreadComponent,
+    RouterOutlet,
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
@@ -22,5 +29,12 @@ export class MainComponent {
 
   toggleThreadVisibility(): void {
     this.isThreadHidden.update((value) => !value);
+  }
+
+  get gridClasses() {
+    return {
+      'workspace-hidden': this.isWorkspaceHidden(),
+      'thread-hidden': this.isThreadHidden(),
+    };
   }
 }
