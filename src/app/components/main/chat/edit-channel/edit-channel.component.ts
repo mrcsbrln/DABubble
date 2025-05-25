65a4b971055856c036af4c45e43ffc45645a43b7
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-05-25 18:21:50 +0200

    progress edit channel component

--- src/app/components/main/chat/edit-channel/edit-channel.component.ts
+++ src/app/components/main/chat/edit-channel/edit-channel.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'app-edit-channel',
   imports: [],
   templateUrl: './edit-channel.component.html',
-  styleUrl: './edit-channel.component.scss'
+  styleUrl: './edit-channel.component.scss',
 })
 export class EditChannelComponent {
+  closeDialog = output<void>();
+  channelName = input<string>();
 
+  isHovering = signal(false);
+
+  closeIconSrc = 'img/close.svg';
+  closeIconHoverSrc = 'img/close-hover.svg';
+
+  onCloseDialog() {
+    this.closeDialog.emit();
+  }
 }
