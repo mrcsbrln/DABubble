74f26114958fc002652d716258d203a69599198f
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-05-30 16:15:10 +0200

    progress direct messages

--- src/app/components/main/direct-message/direct-message.component.ts
+++ src/app/components/main/direct-message/direct-message.component.ts
@@ -1,11 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { UserProfileComponent } from '../user-profile/user-profile.component';
+import { UserService } from '../../../services/user.service';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-direct-message',
-  imports: [],
+  imports: [ReactiveFormsModule, UserProfileComponent],
   templateUrl: './direct-message.component.html',
-  styleUrl: './direct-message.component.scss'
+  styleUrl: './direct-message.component.scss',
 })
 export class DirectMessageComponent {
+  userService = inject(UserService);
 
+  form = new FormGroup({
+    content: new FormControl('', [
+      Validators.required,
+      Validators.minLength(1),
+    ]),
+  });
+
+  isUserProfileDialogOpen = signal(false);
+  selectedMemberId = signal('');
+
+  isHovering = signal(false);
+  isEmojiPickerOpen = signal(false);
+
+  emojis: string[] = [
+    '😀',
+    '😂',
+    '😢',
+    '🤓',
+    '😱',
+    '👍',
+    '👌',
+    '👋',
+    '🎉',
+    '🚀',
+    '🙏',
+    '✅',
+  ];
+
+  getSelectedUser() {
+    //
+  }
+
+  toggleUserProfileDialog() {
+    this.isUserProfileDialogOpen.update((value) => !value);
+  }
+
+  toggleEmojiPicker(event: MouseEvent) {
+    event.stopPropagation();
+    this.isEmojiPickerOpen.update((open) => !open);
+  }
+
+  addEmojiToInput(emoji: string) {
+    const current = this.form.controls.content.value || '';
+    this.form.controls.content.setValue(current + emoji);
+    this.isEmojiPickerOpen.set(false);
+  }
+
+  onSubmit() {
+    //
+  }
 }
