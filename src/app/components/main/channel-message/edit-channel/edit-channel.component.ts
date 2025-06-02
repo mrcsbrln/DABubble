4bbaeb1206fa6dbec15cd79e058dccbb4efd151a
Daniele Dona <d.dona01@gmail.com>
2025-06-02 13:39:39 +0200

    edit channel progress added focus

--- src/app/components/main/channel-message/edit-channel/edit-channel.component.ts
+++ src/app/components/main/channel-message/edit-channel/edit-channel.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output, signal, effect} from '@angular/core';
+import { Component, input, output, signal, effect, ViewChild, ElementRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -37,18 +37,27 @@ export class EditChannelComponent {
     const newValue = (event.target as HTMLInputElement).value;
     this.inputNameValue.set(newValue);
   }
+
   onInputDescription(event: Event) {
     const newValue = (event.target as HTMLTextAreaElement).value;
     this.inputDescriptionValue.set(newValue);
   }
 
   isInputNameReadonly = signal(true);
+  @ViewChild('channelNameInput') channelNameInputRef!: ElementRef<HTMLInputElement>;
   confirmChannelNameEdit() {
     this.isInputNameReadonly.update((currentValue) => !currentValue);
+    if (!this.isInputNameReadonly() && this.channelNameInputRef) {
+      setTimeout(() => this.channelNameInputRef.nativeElement.focus(), 0);
+    }
   }
 
   isInputDescriptionReadonly = signal(true);
+  @ViewChild('channelDescriptionTextarea') channelDescriptionTextareaRef!: ElementRef<HTMLTextAreaElement>;
   confirmChannelDescriptionEdit() {
     this.isInputDescriptionReadonly.update((currentValue) => !currentValue);
+    if (!this.isInputDescriptionReadonly() && this.channelDescriptionTextareaRef) {
+      setTimeout(() => this.channelDescriptionTextareaRef.nativeElement.focus(), 0);
+    }
   }
 }
