3004d5e4483ad83faa14f0e2bf6fd50345cb8a0c
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-06-13 15:01:13 +0200

    update channel-messages interface

--- src/app/interfaces/channel-message.interface.ts
+++ src/app/interfaces/channel-message.interface.ts
@@ -1,11 +1,11 @@
-import { FieldValue } from '@angular/fire/firestore';
+import { FieldValue, Timestamp } from '@angular/fire/firestore';
 import { Reaction } from './reaction.interface';
 
 export interface ChannelMessage {
   id: string; // Eindeutige ID der Nachricht (Firestore Document ID)
   senderId: string; // UID des Absenders (Referenz zu UserProfile.uid)
   content: string; // Textinhalt der Nachricht
-  timestamp: FieldValue; // Zeitstempel, wann die Nachricht gesendet wurde
+  timestamp: FieldValue | Timestamp; // Zeitstempel, wann die Nachricht gesendet wurde
   channelId?: string; // ID des Channels, zu dem die Nachricht gehört (wenn es eine Channel-Nachricht ist)
   parentMessageId?: string; // ID der Ursprungsnachricht, wenn diese Nachricht Teil eines Threads ist (optional)
   reactions?: Reaction[]; // Array der Reaktionen auf diese Nachricht (optional)
