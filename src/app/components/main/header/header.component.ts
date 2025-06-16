59da19ca1e378b92f5674824c8442604f4c5e087
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-06-16 22:46:24 +0200

    set up searchbar dialog

--- src/app/components/main/header/header.component.ts
+++ src/app/components/main/header/header.component.ts
@@ -2,10 +2,12 @@ import { Component, inject, signal } from '@angular/core';
 import { AuthService } from '../../../services/auth/auth.service';
 import { UserService } from '../../../services/user.service';
 import { CurrentUserProfileComponent } from '../current-user-profile/current-user-profile.component';
+import { FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
-  imports: [CurrentUserProfileComponent],
+  imports: [CurrentUserProfileComponent, ReactiveFormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
@@ -16,10 +18,22 @@ export class HeaderComponent {
   arrowDownOpen = signal(false);
   userProfileDialogOpen = signal(false);
 
+  searchBarInput = new FormControl('');
+  isSearchDialogOpen = signal(false);
+
+  checkForAt() {
+    const searchBarValue = this.searchBarInput.value || '';
+    this.isSearchDialogOpen.set(searchBarValue?.includes('@'));
+  }
+
   getCurrentUser() {
     return this.userService
       .users()
       .find((user) => user.uid === this.authService.currentUser()?.uid);
+  }
+
+  getUsers() {
+    return this.userService.users();
   }
 
   toogleArrowDown() {
