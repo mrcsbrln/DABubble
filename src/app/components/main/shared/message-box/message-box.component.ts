ddf7b997d33e065dd779fdbf2e3d71487d0ea89e
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-06-19 19:58:18 +0200

    create message-box component

---
+++ src/app/components/main/shared/message-box/message-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-box',
+  imports: [],
+  templateUrl: './message-box.component.html',
+  styleUrl: './message-box.component.scss'
+})
+export class MessageBoxComponent {
+
+}
