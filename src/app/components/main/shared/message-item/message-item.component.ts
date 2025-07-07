4c97e5a726ebbf420a2ea8b5df6c024ef3f4c32f
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-07-07 11:04:59 +0200

    set up reusable message item component

---
+++ src/app/components/main/shared/message-item/message-item.component.ts
@@ -0,0 +1,129 @@
+import {
+  Component,
+  inject,
+  input,
+  LOCALE_ID,
+  output,
+  signal,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DirectMessage } from '../../../../interfaces/direct-message.interface';
+import { ChannelMessage } from '../../../../interfaces/channel-message.interface';
+import { Timestamp } from '@angular/fire/firestore';
+import { DatePipe, registerLocaleData } from '@angular/common';
+import { AuthService } from '../../../../services/auth/auth.service';
+import { ChannelMessageService } from '../../../../services/channel-message.service';
+import { DirectMessageService } from '../../../../services/direct-message.service';
+import { UserService } from '../../../../services/user.service';
+import localeDe from '@angular/common/locales/de';
+
+registerLocaleData(localeDe);
+
+@Component({
+  selector: 'app-message-item',
+  imports: [CommonModule, DatePipe],
+  templateUrl: './message-item.component.html',
+  styleUrl: './message-item.component.scss',
+  providers: [{ provide: LOCALE_ID, useValue: 'de' }],
+})
+export class MessageItemComponent {
+  private authService = inject(AuthService);
+  private channelMessageService = inject(ChannelMessageService);
+  private directMessageService = inject(DirectMessageService);
+  private userService = inject(UserService);
+
+  message = input<ChannelMessage | DirectMessage>();
+  openThread = output();
+
+  isReactionHovered = signal(false);
+  isAnswerHovered = signal(false);
+  isDotsHovered = signal(false);
+  reactionVisibleId = signal<string | null>(null);
+
+  getDateOfMessage(): Date {
+    const timestamp = this.message()?.timestamp;
+
+    if (timestamp instanceof Timestamp) {
+      return timestamp.toDate();
+    }
+
+    return new Date(0);
+  }
+
+  getSenderOfMessage() {
+    const message = this.message();
+    if (!message) return;
+
+    if ('senderId' in message) {
+      return this.userService
+        .users()
+        .find((user) => user.uid === message.senderId);
+    } else if ('participantIds' in message) {
+      return this.userService
+        .users()
+        .find((user) => user.uid === message.participantIds[0]);
+    } else return;
+  }
+
+  isMessageFromCurrentUser(): boolean {
+    const message = this.message();
+    const currentUserId = this.authService.currentUser()?.uid;
+
+    if (!message || !currentUserId) return false;
+
+    if ('senderId' in message) {
+      // DirectMessage
+      return message.senderId === currentUserId;
+    }
+
+    if ('participantIds' in message) {
+      // ChannelMessage
+      return message.participantIds[0] === currentUserId;
+    }
+
+    return false;
+  }
+
+  isToday(): boolean {
+    const message = this.message();
+    if (!message?.timestamp || !(message?.timestamp instanceof Timestamp)) {
+      return false;
+    }
+
+    const messageDate = message.timestamp.toDate();
+    const today = new Date();
+
+    return (
+      messageDate.getDate() === today.getDate() &&
+      messageDate.getMonth() === today.getMonth() &&
+      messageDate.getFullYear() === today.getFullYear()
+    );
+  }
+
+  onMouseEnterMessage() {
+    const message = this.message();
+    if (message?.id) {
+      this.reactionVisibleId.set(message.id);
+    }
+  }
+
+  onMouseLeaveMessage() {
+    this.reactionVisibleId.set(null);
+  }
+
+  onOpenThread() {
+    this.openThread.emit();
+  }
+
+  setParentDirectMessageId(id: string) {
+    const message = this.message();
+
+    if (!message) return;
+
+    if ('senderId' in message) {
+      this.directMessageService.parentDirectMessageId.set(id);
+    } else if ('participantIds' in message) {
+      return this.channelMessageService.parentChannelMessageId.set(id);
+    }
+  }
+}
