71f80ea10c60008b010588e3bacf80bd11e22b37
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-08-05 14:55:47 +0200

    set up autocommplete input for usernames in new message comp

--- src/app/components/main/new-message/new-message.component.ts
+++ src/app/components/main/new-message/new-message.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { MessageBoxComponent } from '../shared/message-box/message-box.component';
 import { ChannelService } from '../../../services/channel.service';
 import { UserService } from '../../../services/user.service';
@@ -12,4 +12,30 @@ import { UserService } from '../../../services/user.service';
 export class NewMessageComponent {
   private channelService = inject(ChannelService);
   private userService = inject(UserService);
+
+  inputText = signal('');
+  selected = signal(false);
+
+  filteredUsernames = computed(() => {
+    if (this.selected()) return [];
+    const allUsernames = computed(() =>
+      this.userService
+        .users()
+        .map((u) => u.displayName)
+        .filter(Boolean)
+    );
+    const query = this.inputText().toLowerCase();
+    return allUsernames().filter((name) => name.toLowerCase().includes(query));
+  });
+
+  onInputChange(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.inputText.set(value);
+    this.selected.set(false);
+  }
+
+  selectSuggestion(username: string) {
+    this.inputText.set(username);
+    this.selected.set(true);
+  }
 }
